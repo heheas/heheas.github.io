66abe83f871e2852fcd7e71bb5cf0a745904425a
heheas <hehe.andrew@gmail.com>
2025-02-26 15:34:31 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,13 +1,13 @@
 function nextSlide() {
   let focus = $('.goober-slider li.focus');
   let nextFocus = focus.next().length ? focus.next() : $('.goober-slider li').first();
-  nextFocus.addClass("focus");
-  focus.removeClass("focus");
+  nextFocus.addClass("focus rightToLeft");
+  focus.removeClass("focus leftToRight");
 }
 
 function prevSlide() {
   let focus = $('.goober-slider li.focus');
   let nextFocus = focus.prev().length ? focus.prev() : $('.goober-slider li').last();
-  nextFocus.addClass("focus");
-  focus.removeClass("focus");
+  nextFocus.addClass("focus leftToRight");
+  focus.removeClass("focus rightToLeft");
 }
