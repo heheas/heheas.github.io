89f95126c12cf36e5f0a5b4e51257c358ca954ba
heheas <hehe.andrew@gmail.com>
2025-02-26 13:51:10 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,25 +1,5 @@
-let swiper;
-function init() {
-  console.log("Swiper");
-  swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+function nextSlide() {
+  let focus = $('.goober-slider li.focus');
+  focus.next().addClass("focus");
+  focus.removeClass("focus");
 }
