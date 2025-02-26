637b27f121456b325042e62a722a10014c717e69
heheas <hehe.andrew@gmail.com>
2025-02-26 10:03:30 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,11 +1,1 @@
-	import Splide from '@splidejs/splide';
-		
-	function init() {
-		var splide = new Splide( '.splide', {
-		  type    : 'loop',
-		  perPage : 3,
-		  autoplay: true,
-		} );
-		
-		splide.mount();
-	}
+
