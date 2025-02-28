a1815d65cd14e93f3dce4565d284c6b2c7cab967
heheas <hehe.andrew@gmail.com>
2025-02-28 13:26:01 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,16 +2,16 @@ function nextSlide() {
   let focus = $('.goober-slider li.focus');
   let prevPanel = focus.prev().length ? focus.prev() : $('.goober-slider li').last();
   let nextPanel = focus.next().length ? focus.next() : $('.goober-slider li').first();
-  prevPanel.removeClass("left").addClass("right").css("display", "inline-block");
+  prevPanel.removeClass("left").addClass("right").prop("display", "none");
   focus.removeClass("focus").addClass("left")
-  nextPanel.removeClass("right").addClass("focus").removeProp("display");
+  nextPanel.removeClass("right").addClass("focus").prop("display", "inline-block");
 }
 
 function prevSlide() {
   let focus = $('.goober-slider li.focus');
   let prevFocus = focus.prev().length ? focus.prev() : $('.goober-slider li').last();
   let nextFocus = focus.next().length ? focus.next() : $('.goober-slider li').first();
-  prevFocus.removeClass("left").addClass("focus").css("display", "inline-block");
+  prevFocus.removeClass("left").addClass("focus").prop("display", "inline-block");
   focus.removeClass("focus").addClass("right");
-  nextFocus.removeClass("right").addClass("left").css("display", "none");
+  nextFocus.removeClass("right").addClass("left").prop("display", "none");
 }
