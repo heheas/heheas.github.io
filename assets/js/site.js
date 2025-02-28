4769205acc1fb43efd5f3acdeb91e48ab5365b50
heheas <hehe.andrew@gmail.com>
2025-02-28 13:14:42 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,17 +1,17 @@
 function nextSlide() {
   let focus = $('.goober-slider li.focus');
-  let prevFocus = focus.prev().length ? focus.prev() : $('.goober-slider li').last();
-  let nextFocus = focus.next().length ? focus.next() : $('.goober-slider li').first();
-  focus.removeClass("focus").addClass("left").show();
-  nextFocus.removeClass("right").addClass("focus").hide();
-  prevFocus.removeClass("left").addClass("right").hide();
+  let prevPanel = focus.prev().length ? focus.prev() : $('.goober-slider li').last();
+  let nextPanel = focus.next().length ? focus.next() : $('.goober-slider li').first();
+  prevPanel.removeClass("left").addClass("right").css("display", "none");
+  focus.removeClass("focus").addClass("left")
+  nextPanel.removeClass("right").addClass("focus").removeProperty("display");
 }
 
 function prevSlide() {
   let focus = $('.goober-slider li.focus');
   let prevFocus = focus.prev().length ? focus.prev() : $('.goober-slider li').last();
   let nextFocus = focus.next().length ? focus.next() : $('.goober-slider li').first();
-  focus.removeClass("focus").addClass("left").show();
-  nextFocus.removeClass("left").addClass("right").hide();
-  prevFocus.removeClass("right").addClass("focus").hide();
+  prevFocus.removeClass("left").addClass("focus").removeProperty("display");
+  focus.removeClass("focus").addClass("right");
+  nextFocus.removeClass("right").addClass("left").css("display", "none");
 }
