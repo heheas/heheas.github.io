ff0fedef880a0c8640a87e220bfda0e8a93c61f7
heheas <hehe.andrew@gmail.com>
2025-03-07 11:42:35 -0500

    Header update (#4)
    * Merge Main into HeaderUpdate (#3)
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Create site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update site.js
    
    * Update index.html
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update index.html
    
    * Update index.html
    
    * Update site.css
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update index.html
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update index.html
    
    * Update site.js
    
    * Update index.html
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update index.html
    
    * Update site.js
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update index.html
    
    * Update site.css
    
    * Update site.js
    
    * Update site.js
    
    * Update site.css
    
    * Update site.css
    
    * Update index.html
    
    * Update site.css
    
    * Update index.html
    
    * Update index.html
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update index.html
    
    * Update site.css
    
    * Update index.html
    
    * Update index.html
    
    * Update site.css
    
    * Update index.html
    
    * Update site.css
    
    * Update site.css
    
    * Update site.js
    
    * Update site.js
    
    * Update site.css
    
    * Update site.css
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update site.css
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update site.css
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update index.html
    
    * Update site.js
    
    * Update site.js
    
    * Update index.html
    
    * Update site.js
    
    * Update site.js
    
    * Update site.js
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update site.css
    
    * Update site.css
    
    * Update index.html
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css
    
    * Update index.html
    
    * Update site.css
    
    * Update site.css
    
    * Update site.css

---
+++ assets/js/site.js
@@ -0,0 +1,34 @@
+function nextSlide() {
+  let focusPanel = $('.goober-slider li.focus');
+  $(focusPanel).siblings().removeClass("left").addClass("right").css("visibility", "hidden");
+  let prevPanel = focusPanel.prev().length ? focusPanel.prev() : $('.goober-slider li').last();
+  let nextPanel = focusPanel.next().length ? focusPanel.next() : $('.goober-slider li').first();
+  //prevPanel.removeClass("left").addClass("right").css("visibility", "hidden");
+  focusPanel.removeClass("focus").addClass("left");
+  nextPanel.removeClass("right").addClass("focus").css("visibility", "visible");
+  
+  
+  $('.goober-slider-prev').attr('disabled', true);
+  $('.goober-slider-next').attr('disabled', true);
+  setTimeout(function(){
+        $('.goober-slider-prev').removeAttr('disabled');
+        $('.goober-slider-next').removeAttr('disabled');
+    }, 2000)
+}
+
+function prevSlide() {
+  let focusPanel = $('.goober-slider li.focus');
+  $(focusPanel).siblings().removeClass("right").addClass("left").css("visibility", "hidden");
+  let prevPanel = focusPanel.prev().length ? focusPanel.prev() : $('.goober-slider li').last();
+  let nextPanel = focusPanel.next().length ? focusPanel.next() : $('.goober-slider li').first();
+  prevPanel.removeClass("left").addClass("focus").css("visibility", "visible");
+  focusPanel.removeClass("focus").addClass("right");
+  //nextPanel.removeClass("right").addClass("left").css("visibility", "hidden");
+
+  $('.goober-slider-prev').attr('disabled', true);
+  $('.goober-slider-next').attr('disabled', true);
+  setTimeout(function(){
+        $('.goober-slider-prev').removeAttr('disabled');
+        $('.goober-slider-next').removeAttr('disabled');
+    }, 2000)
+}
