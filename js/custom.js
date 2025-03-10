97c3c2d0ba9d61f51586b74baeb51512799a4095
heheas <hehe.andrew@gmail.com>
2025-03-10 14:30:58 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ function getYear() {
 getYear();
 
 function toggleMenu() {
-    $('.navbar-toggler').attr('aria-expanded', !Boolean($('.menuToggle').attr('aria-expanded')));
+    $('.navbar-toggler').attr('aria-expanded', !!!($('.menuToggle').attr('aria-expanded')));
     $('.menuToggle').toggleClass('menuToggleExpand');
 }
 /*
