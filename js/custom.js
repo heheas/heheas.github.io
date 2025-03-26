a3a7a18ef5ba3bd948bbf7235291328f359d8a78
heheas <hehe.andrew@gmail.com>
2025-03-26 10:36:45 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -14,14 +14,16 @@ function loadCookies() {
     var cookieSet = document.cookie.split(';');
     
     console.log("split cookies: " + JSON.stringify(cookieSet));
-    cookieSet.forEach( (cookie) => {
-        console.log("cookie 1: " + cookie);
-        if ( cookie.includes('=') ) {
-            var cookieKeyValue = cookie.split('=');
-            console.log("split cookie data: " + JSON.stringify(cookieKeyValue));
-            cookies.set(cookieKeyValue[0], cookieKeyValue[1]);
-        }
-    };
+    if (cookieSet) {
+        cookieSet.forEach( (cookie) => {
+            console.log("cookie 1: " + cookie);
+            if ( cookie.includes('=') ) {
+                var cookieKeyValue = cookie.split('=');
+                console.log("split cookie data: " + JSON.stringify(cookieKeyValue));
+                cookies.set(cookieKeyValue[0], cookieKeyValue[1]);
+            }
+        };
+    }
 }
 
 // to get current year
