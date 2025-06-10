d5d10e26ac8f3d2c8b7f1993d4d2eac1f851dc1b
heheas <hehe.andrew@gmail.com>
2025-06-10 11:52:30 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -127,13 +127,13 @@ function toggleInstructions() {
 function toggleMailingList() {
     var mailingList = $('#mailingList');
     if ($(mailingList).hasClass('Expanded')) {
-        setTimeout(() => {
+        //setTimeout(() => {
             $(mailingList).find('h5').html("Join our mailing list!");
-        }, 250ms);
+        //}, 250ms);
     } else {
-        setTimeout(() => {
+        //setTimeout(() => {
             $(mailingList).find('h5').html('<i class="fa-solid fa-caret-down" style="font-size:xx-large;"></i>');
-        }, 250ms);
+        //}, 250ms);
 
     }
     $(mailingList).toggleClass('Expanded');
